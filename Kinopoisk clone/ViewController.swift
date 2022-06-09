2d756bf51f8901e960e80d2190de4702b91f64cb
ofigensky <bulletproofgleb@icloud.com>
2022-06-09 14:43:42 +0300

    Initial Commit

---
+++ Kinopoisk clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kinopoisk clone
+//
+//  Created by Gleb Glushok on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
